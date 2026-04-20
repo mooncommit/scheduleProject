f4ddf4559c908262d9c2f747e74cecc5a3967cd1
mooncommit <mooncommit@users.noreply.github.com>
2026-04-20 13:10:34 +0900

    Feat : user Entity 생성
    유저명, 이메일, 작성일, 수정일 필드

---
+++ src/main/java/com/example/deepeningschedule/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.deepeningschedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
