885c0590e2a4749fb8026d179a1ea592b8c053e3
mooncommit <mooncommit@users.noreply.github.com>
2026-04-20 12:40:32 +0900

    init

---
+++ src/test/java/com/example/deepeningschedule/DeepeningScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepeningschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepeningScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
